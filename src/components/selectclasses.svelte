6eb967bc3c583aef4581f2409c66b65a2cb14793
avidcoder123 <umar.tli.edu@gmail.com>
2023-12-19 15:10:49 +0000

    1/2 select page

--- src/components/selectclasses.svelte
+++ src/components/selectclasses.svelte
@@ -15,6 +15,8 @@
 
     let school = ""
 
+    let loading = false
+
     appwriteDatabases.listDocuments(DB_ID, COLLECTION.Class_User, [
         Query.equal("User", user)
     ]).then(res => res.documents.forEach(doc => {
@@ -28,7 +30,8 @@
     .then(doc => {
         school = doc.School
         return appwriteDatabases.listDocuments(DB_ID, COLLECTION.Classes, [
-            Query.equal("School", school)
+            Query.equal("School", school),
+            Query.orderAsc("Name")
         ])
     })
     .then(res => {
@@ -55,6 +58,7 @@
     }
 
     function updateClasses() {
+        loading = true
         appwriteDatabases.listDocuments(DB_ID, COLLECTION.Class_User, [
             Query.equal("User", user)
         ]).then(res => {
@@ -69,28 +73,33 @@
                     Class: c,
                     User: user
                 })
-            ))
-        }).then(() => window.location.href = "/dashboard")
+            )).then(() => loading = false)
+        })
     }
 </script>
-<div class="flex flex-col items-center 2xl:scale-125 pt-10 gap-4">
-    <p class="text-3xl text-center font-bold">Select your classes</p>
-    <button class="h-12 bg-blue-500 rounded-md w-44" on:click={() => {
+<div class="flex flex-col items-center 2xl:scale-125 pt-10 gap-4 h-screen">
+    <p class="text-3xl text-center font-bold text-white">Select your classes</p>
+    <button class="text-white rounded-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm px-6 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" 
+    on:click={() => {
         const modal = document.getElementById('newclassmodal');
         if (modal instanceof HTMLDialogElement && typeof modal.showModal === 'function') {
             modal.showModal();
         }
     }}>+ New Class</button>
-    <div>
+    <div class="flex flex-col gap-3">
         {#each classes as c}
-            <label class="cursor-pointer label">
-                <input value={c.$id} bind:group={selectedClasses} type="checkbox" class="checkbox checkbox-info checkbox-lg" />
-                <span class="label-text ml-2 mr-auto text-lg text-left">{c.Name}</span>
+            <label class="cursor-pointer label flex-row justify-center">
+                <input value={c.$id} bind:group={selectedClasses} on:change={updateClasses} type="checkbox" class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                <span class="text-lg ms-2 font-medium text-gray-900 dark:text-gray-300">{c.Name}</span>
             </label>
-        {/each}
-        
+        {/each}    
     </div>
-    <button class="bg-gray-400 rounded-md w-36 h-12" on:click={updateClasses} disabled={selectedClasses.length == 0}>Save</button>
+    <button disabled={loading} on:click={() => location.href = "/dashboard"} type="button" class="flex flex-row gap-2 disabled:opacity-50 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 enabled:dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+        <svg class="w-3.5 h-3.5 rotate-180 my-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+        Back to dashboard
+    </button>
 </div>
 <dialog id="newclassmodal" class="modal">
     <form method="dialog" class="modal-box bg-gray-200 flex flex-col gap-4">
