01d028bb9a2073fc9a8485a57d1185c96c2a2be1
avidcoder123 <umar.tli.edu@gmail.com>
2023-12-28 01:28:40 +0000

    sdf

--- src/components/newassignment.svelte
+++ src/components/newassignment.svelte
@@ -64,6 +64,8 @@
             alert(e.message)
         })
     }
+
+    let hoverMenu = false
 </script>
 <div class=" w-screen h-screen">
 <h1 class="text-center text-5xl pt-16 text-white">Make a New Assignment</h1>
@@ -84,7 +86,7 @@
                 {/if}
  
                 <div class="flex flex-col gap-y-2 w-96 sm:w-[32rem]">
-                    <input type="text" bind:value={question.question} placeholder="Question Text" class="input input-bordered input-primary w-96 sm:w-full sm:max-w-lg" />
+                    <input type="text" bind:value={question.question} placeholder="Question Text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                     {#if question.is_file}
                         <!-- <input bind:files={question.files} on:change={() => question.answer = Date.now().toString()} accept="image/png, image/jpeg" type="file" class="btn bg-base-content w-96 h-96 sm:w-full sm:max-w-lg" /> -->
                         <div class="flex w-full pt-10 items-center justify-center bg-grey-lighter">
@@ -105,22 +107,30 @@
                             {/each}
                         {/if}
                     {:else}
-                        <textarea bind:value={question.answer} class="textarea textarea-primary w-full sm:max-w-lg h-32" placeholder="Question Answer"></textarea>
+                        <textarea bind:value={question.answer} class="h-32 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Question Answer"></textarea>
                     {/if}
                 </div>
             </div>
         {/each}
     </div>
-    <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" tabindex="0" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">+ New Question</button>
-    <div id="dropdown" class="z-10 hiddfd0IPK:/n bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-          <li>
-            <a on:click={addFree} class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Multiple Choice/Free Response</a>
-          </li>
-          <li>
-            <a on:click={addFile} class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">File Upload</a>
-          </li>
-        </ul>
+    <div
+    on:mouseenter={() => hoverMenu = true}
+    on:mouseleave={() => hoverMenu = false}
+    class="mx-auto w-44 flex flex-col items-center">
+        <button 
+        tabindex="0" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">+ New Question</button>
+        {#if hoverMenu}
+            <div class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                <li>
+                    <a on:click={addFree} class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Multiple Choice/Free Response</a>
+                </li>
+                <li>
+                    <a on:click={addFile} class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">File Upload</a>
+                </li>
+                </ul>
+            </div>
+        {/if}
     </div>
     <button on:click={submit} class="btn btn-info" class:btn-disabled={disabled}>Create Assignment</button>
 </div>
